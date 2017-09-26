94b51271868eba2c25b5e2c6f64bbe9e4d22e5e9
Tasnim Rahman <www.trjarin@gmail.com>
2017-09-26 07:05:34 +0600

    testSimpleGene method added which will test findSimpleGene method by providing copuble of dna sequences

--- Part1.java
+++ Part1.java
@@ -1,28 +1,61 @@
 
-
 public class Part1
 {
-    String findGene(String DNA)
- {
-   String result = "";   
-   int startIndex = DNA.indexOf("ATG");   
-   if(startIndex == -1)
-   return result;
-   int stopIndex = DNA.indexOf("TAA", startIndex + 3);
-   if(stopIndex == -1)
-   return result;
-   if((stopIndex - startIndex) % 3 != 0)
-    return "";
-   result = DNA.substring(startIndex,stopIndex+3);
-   return result;
- }
- void testGene()
- {
-  String DNAStrand;
-  String gene;
-  DNAStrand = "ACTGCTAATATGCTATAA";
-  gene = findGene(DNAStrand);
-  System.out.println("DNA: " + DNAStrand + "\nGENA: " + gene);
   
+    public String findSimpleGene(String dna)
+    {
+       int startIndex = dna.indexOf("ATG");
+       if(startIndex == -1)
+       {
+     
+           return "";
+        
+       }
+      int stopIndex = dna.indexOf("TAA",startIndex +3);
+      
+      while(stopIndex != -1){
+           if((stopIndex - startIndex)%3  == 0){
+               return dna.substring(startIndex , stopIndex +3);
+            
+            }
+        else
+           stopIndex = dna.indexOf("TAA", stopIndex +1);
+        
+        
+        
+        }
+        return " ";
+    }
+
+ 
+    void testSimpleGene(){
+  
+        String dna = "ATAGTATCAACCTAAGCT";
+        String gene = findSimpleGene(dna);
+        System.out.println("DNA :" +dna+"\n" + "Gene :" +gene);
+   
+   
+        dna = "AGTAGCCTATGAACTAC";
+        gene = findSimpleGene(dna);
+        System.out.println("DNA :" +dna+"\n" + "Gene :" +gene);
+   
+   
+        dna = "ATGCATTACTGCCAATACGA";
+        gene = findSimpleGene(dna);
+        System.out.println("DNA :" +dna+"\n" + "Gene :" +gene);
+   
+   
+        dna = "ATGTTACGATAA";
+        gene = findSimpleGene(dna);
+        System.out.println("DNA :" +dna+"\n" + "Gene :" +gene);
+    
+        
+        dna = "ATGTTACGTAA";
+        gene = findSimpleGene(dna);
+        System.out.println("DNA :" +dna+"\n" + "Gene :" +gene);
+        
+        
+ 
    }
+   
 }
